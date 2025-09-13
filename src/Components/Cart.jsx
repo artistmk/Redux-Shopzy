8734303f7adfe4da9724def7ec6f2806c8f30b5d
artistmk <firewithmk172000@gmail.com>
2025-09-13 14:06:26 +0530

    done2

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,74 +1,73 @@
 import { useSelector, useDispatch } from 'react-redux'
-import {clearCart, selectCartItem} from '../redux/cartSlice/cartSlice'
+import { clearCart, selectCartItem } from '../redux/cartSlice/cartSlice'
 import { Link } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Cart = () => {
-    
-    const dispatch = useDispatch();
-    const cartItem = useSelector(selectCartItem)
+  const dispatch = useDispatch();
+  const cartItem = useSelector(selectCartItem);
 
-    let myStyle = {
-      minHeight: "70vh",
-      margin: "40px auto"
-    }
+  let myStyle = {
+    minHeight: "70vh",
+    margin: "40px auto"
+  };
 
   return (
     <>
-    <ToastContainer/>
-     <div style={myStyle}>
+      <ToastContainer />
+      <div style={myStyle}>
 
-       <div className="container text-center" style={{width: "700px"}}>
-         
+        <div className="container my-4">
           {cartItem.length === 0 ? (
-             <>
-       <div className='text-center my-5'>
-          <h1> Your Cart is Empty :{"("} </h1>
-          <Link to={"/"} className="btn btn-dark my-3" style={{fontSize: "15px"}}>Continue Shopping...</Link>
-        </div>
-        </>
-      ) :
-      cartItem.map((item) => {
-         return (
-            <div key={item.id} className="container my-5">
-                         
-                        <div className="card mb-3 text-center" style={{width: "650px"}}>
-                <div className="row g-0">
-               <div className="col-md-4">
-
-            <div className='p-3'>
-         <img src={item.imgSrc} className="img-fluid rounded-start" alt="..."/>
-          </div>
-    </div>
-
-               <div className="col-md-8">
-             <div className="card-body my-3">
-            <h5 className="card-title">{item.title}</h5>
-         <p className="card-text">{item.description}</p>
-
-     <div className='container text-center my-4 flexi1' style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "3rem"}}>
-        <button className='btn btn-dark'>{item.price} ₹</button>
-        <button className="btn btn-primary">BUY NOW</button>
-         <button className='btn btn-danger'>Remove</button>
+            <div className="text-center my-5">
+              <h1>Your Cart is Empty :(</h1>
+              <Link to="/" className="btn btn-dark my-3" style={{ fontSize: "15px" }}>
+                Continue Shopping...
+              </Link>
             </div>
+          ) : (
+            <div className="row justify-content-center">
 
-              </div>
-           </div>
-       </div>
-         </div>
-     </div>
-                )
-               })
-            }
+              {cartItem.map((item) => (
 
-          {cartItem.length !=0 && (
-             <div className="container text-center my-5" style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "3rem"}}>
-           <div className="btn btn-dark">Check Out</div>
-           <div onClick={() => {dispatch(clearCart())
-               toast.success('Cart Cleared!', {
-                  position: "top-right",
-                  autoClose: 1500,
+                <div key={item.id} className="col-12 col-md-10 col-lg-8 my-3">
+
+                  <div className="card mb-3">
+                    <div className="row g-0">
+
+                      <div className="col-md-4 d-flex justify-content-center align-items-center p-3">
+                        <img src={item.imgSrc} className="img-fluid rounded-start" alt={item.title}/>
+                      </div>
+
+                      <div className="col-md-8">
+                        <div className="card-body text-center my-3">
+                          <h5 className="card-title">{item.title}</h5>
+                          <p className="card-text">{item.description}</p>
+
+                          <div className="d-flex justify-content-center align-items-center gap-3 my-4">
+                            <button className="btn btn-dark">{item.price} ₹</button>
+                            <button className="btn btn-primary">BUY NOW</button>
+                            <button className="btn btn-danger">Remove</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {cartItem.length !== 0 && (
+            <div className="container text-center my-5 d-flex justify-content-center gap-3">
+              <div className="btn btn-dark">Check Out</div>
+              <div
+                onClick={() => {
+                  dispatch(clearCart());
+                  toast.success('Cart Cleared!', {
+                    position: "top-right",
+                    autoClose: 1500,
                     hideProgressBar: false,
                     closeOnClick: false,
                     pauseOnHover: true,
@@ -76,13 +75,15 @@ const Cart = () => {
                     progress: undefined,
                     theme: "light",
                   });
-               }} className="btn btn-danger">Clear All</div>
-       </div>
-     )}
-    </div>
-     </div>
+                }}
+                className="btn btn-danger" >Clear All</div>
+            </div>
+          )}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
+
